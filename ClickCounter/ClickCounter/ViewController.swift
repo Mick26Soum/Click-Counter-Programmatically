a7b8a0fff242ec391ba9f7deca32e499b3a44d80
Mick Soum <Mick26Soum@gmail.com>
2016-11-26 12:07:19 -0800

    add autosizing for UIButton

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -9,24 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	var counter: Int = 0
+	var clickButton: UIButton!
+	var counterLabel: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		// Create a UIButton
-		let clickButton = UIButton()
+		// UILabel
+		counterLabel = UILabel()
+		counterLabel.frame = CGRect.init(x: 200, y: 200, width: 60, height: 60)
+		counterLabel.text = "0"
+		self.view.addSubview(counterLabel)
+		
+		// UIButton
+		clickButton = UIButton.init(type: .roundedRect)
 		clickButton.setTitle("Click Here", for: .normal)
 		clickButton.setTitleColor(UIColor.white, for: .normal)
-		clickButton.backgroundColor = UIColor.blue
-		clickButton.frame = CGRect(x: 150, y: 300, width: 50, height: 50)
+		clickButton.backgroundColor = UIColor.lightGray
+		clickButton.sizeToFit() //automatically resizes the button
+		clickButton.center = CGPoint(x: 200, y: 300)
 		clickButton.layer.cornerRadius = 5
-		clickButton.invalidateIntrinsicContentSize() // try to auto rezize
-		self.view.addSubview(clickButton)
-		
+		clickButton.addTarget(self, action:#selector(ViewController.incrementCounter), for:UIControlEvents.touchUpInside)
+		self.view.addSubview(clickButton) 
 		
 	}
 	
+	func incrementCounter(){
+		
+		counter = counter + 1
+		counterLabel.text = "\(counter)"
+		
+	}
 	
 
 }
